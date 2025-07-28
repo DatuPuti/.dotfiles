c7c0682735de0bf9b8207669006e9af97bcee2c6
DatuPuti <datuputi1@gamil.com>
2025-07-28 14:09:02 -0400

    added keybinding for copilot chat

--- .config/nvim/lua/plugins/copilotChat.lua
+++ .config/nvim/lua/plugins/copilotChat.lua
@@ -3,6 +3,11 @@ return {
   dependencies = { "github/copilot.vim" },
   config = function()
     require("CopilotChat").setup()
+
+    -- Add key mappings for CopilotChat
+    vim.keymap.set("n", "<leader>cc", function()
+      require("CopilotChat").open()
+    end, { desc = "Open Copilot Chat" })
   end,
 }
 
