08e057706e8e6528adb0b88d6d4ca4421660217a
DatuPuti <datuputi1@gamil.com>
2025-07-29 00:03:08 -0400

    added key bindings to call cmake

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,6 @@
 return {
-    vim.keymap.set("n", "<C-t>", ":belowright 15split | terminal<CR>", {})
+    vim.keymap.set("n", "<C-t>", ":belowright 15split | terminal<CR>", {}),
+    vim.keymap.set("n", "<leader>b", ":!cmake -S . -B build && cmake --build build<CR>", {
+        desc = "Build C++ Project with CMake"
+    })
 }
